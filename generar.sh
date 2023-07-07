19883a9cab3f0c74a24041e0b0383f62718002bb
Octavio Ismael Fernandez <fernandez.octavio.ismael@gmail.com>
2023-07-07 17:25:07 -0300

    subiendo generar.sh

---
+++ generar.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "BIENVENIDO AL GENERADOR DE IMAGENES"
+
+CANT_IMG=$1
+
+curl https://raw.githubusercontent.com/adalessandro/EdP-2023-TP-Final/main/dict.csv > nombres.txt
+#CANT_LINEAS=$(wc -l nombres.txt | cut -d ' ' -f1)
+
+for (( i=0; i<$CANT_IMG; i++ ))
+do
+	NUM_LINEA=$RANDOM
+	while [[ $NUM_LINEA > 96222  ]]
+	do
+		NUM_LINEA=$RANDOM
+	done
+
+	NOMBRE=$(awk -v "variable=$NUM_LINEA" 'NR==variable' nombres.txt | cut -d "," -f 1 | tr " " "_")
+	curl -L https://thispersondoesnotexist.com/ --output img.tmp
+	cp img.tmp $NOMBRE.jpg
+
+	sleep 1
+done
